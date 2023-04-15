c0e7f94a8e15ac40ba72f33114afbbb6da785b17
Danilo Henrique <danilohssantana@gmail.com>
2023-04-15 18:28:43 -0300

    longest_common_prefix

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -1,8 +1,24 @@
 class Solution:
     def longestCommonPrefix(self, strs: list[str]) -> str:
-        print(strs)
+        res = ""
+
+        for i in range(len(strs[0])):  # executa 2 vezes
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+
+            res += strs[0][i]
+
+        return res
 
 
 solve = Solution()
 
-solve.longestCommonPrefix("IV")
+# result = solve.longestCommonPrefix(["dog", "doter", "dogshoe"])
+# result = solve.longestCommonPrefix(["dog", "racecar", "car"])
+# result = solve.longestCommonPrefix(["flower", "flower", "flower", "flower"])
+# result = solve.longestCommonPrefix(["fwer", "fl", "fht"])
+# result = solve.longestCommonPrefix(["flower", "flow", "flight"])
+result = solve.longestCommonPrefix(["ab", "a"])
+
+print(result)
