23e8ba2bf2503d9e5412c37a297e0856c1e9b3a9
Danilo Henrique <danilohssantana@gmail.com>
2024-10-14 16:31:17 -0300

    finishing group anagram problem

--- Arrays/group_anagrams.py
+++ Arrays/group_anagrams.py
@@ -1,10 +1,40 @@
+from collections import defaultdict
+
+
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        print(strs)
+        if len(strs) == 0:
+            return [[""]]
+
+        anagrams: dict[str, list] = {}
+
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+
+            if sorted_word not in anagrams:
+                anagrams[sorted_word] = []
+            anagrams[sorted_word].append(word)
+
+        result = []
+
+        for values in anagrams.values():
+            result.append(values)
+
+        return result
+
+    def groupAnagrams_hastable(self, strs: list[str]) -> list[list[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)
+        return res.values()
 
 
 if __name__ == "__main__":
     solve = Solution()
-    solve.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
-    solve.groupAnagrams([""])
-    solve.groupAnagrams(["a"])
+    # solve.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+    solve.groupAnagrams_hastable(["act", "pots", "tops", "cat", "stop", "hat"])
+    # solve.groupAnagrams([""])
+    # solve.groupAnagrams(["a"])
